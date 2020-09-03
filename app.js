a4a665c1df230155dd52003b42ae78f290ce4c01
Guiala Jean Roger <nigoumijeanroger@gmail.com>
2020-09-02 23:38:53 -0400

    basic start

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+
 const app = express();
+
+const events = [];
+
 
 app.use(bodyParser.json());
 
@@ -10,12 +14,28 @@ app.use(
     '/graphql',
     graphqlHTTP({
         schema: buildSchema(`
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name:String):String
+            createEvent(eventInput: EventInput):Event
         }
 
         schema {
@@ -25,12 +45,19 @@ app.use(
     `),
         rootValue: {
             events: () => {
-                return ['mock', 'test', 'cooking', 'coding']
+                return events;
             },
             createEvent: (args) => {
-                const eventName = args.name;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: new Date().toISOString()
+                };
+                events.push(event);
 
-                return eventName;
+                return event;
             }
         },
         graphiql: true
